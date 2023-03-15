c65c4b2cb3180531bf91188d5e95540273919ccc
floh-bhaptics <floh@fahrenbergers.de>
2023-03-15 11:43:32 +0100

    Testing a first guess at the game.

--- Demeo_bhaptics.cs
+++ Demeo_bhaptics.cs
@@ -7,12 +7,16 @@ using MelonLoader;
 using HarmonyLib;
 using MyBhapticsTactsuit;
 using Boardgame.Haptic;
+using Boardgame.BoardEntities;
+using Boardgame;
+using UnityEngine;
 
 namespace Demeo_bhaptics
 {
     public class Demeo_bhaptics : MelonMod
     {
         public static TactsuitVR tactsuitVr;
+        public static int myNetworkId = 0;
         public override void OnInitializeMelon()
         {
             tactsuitVr = new TactsuitVR();
@@ -26,6 +30,47 @@ namespace Demeo_bhaptics
             public static void Postfix(HapticHand hapticHand, HapticId hapticId)
             {
                 if (hapticId == HapticId.DefaultClickButton) { tactsuitVr.LOG("Right? " + (hapticHand == HapticHand.RightHand).ToString()); }
+            }
+        }
+
+        [HarmonyPatch(typeof(Dice), "OnDiceStopped", new Type[] { typeof(Dice.Outcome), typeof(Vector3), typeof(Quaternion) })]
+        public class bhaptics_DiceStopped
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Dice.Outcome outcome)
+            {
+                if (outcome == Dice.Outcome.Miss) tactsuitVr.LOG("Miss");
+                if (outcome == Dice.Outcome.Hit) tactsuitVr.LOG("Hit");
+                if (outcome == Dice.Outcome.Crit) tactsuitVr.LOG("Crit");
+            }
+        }
+
+        [HarmonyPatch(typeof(Boardgame.BoardEntities.Abilities.Ability), "GenerateAttackDamage", new Type[] { typeof(Piece), typeof(Piece), typeof(Dice.Outcome), typeof(BoardModel), typeof(Piece[]) })]
+        public class bhaptics_GenerateAttackDamage
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Boardgame.BoardEntities.Abilities.Ability __instance, Piece source, Piece mainTarget, Dice.Outcome diceResult)
+            {
+                if (source.networkID == myNetworkId)
+                {
+                    tactsuitVr.LOG("Damaged something!");
+                }
+                if (mainTarget.networkID == myNetworkId)
+                {
+                    if (diceResult == Dice.Outcome.Miss) return;
+                    tactsuitVr.LOG("Got hit!");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Boardgame.Networking.INetworkController), "AllocateNetworkID", new Type[] {  })]
+        public class bhaptics_AllocateNetworkID
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Boardgame.Networking.INetworkController __instance, int __result)
+            {
+                tactsuitVr.LOG("Network ID: " + __result.ToString());
+                myNetworkId = __result;
             }
         }
 
